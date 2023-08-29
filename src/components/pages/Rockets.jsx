7ab994bb6d1fc0e431384581639432291288e519
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-29 12:56:49 +0100

    updated changes on rocket-booking

--- src/components/pages/Rockets.jsx
+++ src/components/pages/Rockets.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets, rocketsReserve } from '../../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <div className="inside">Rockets</div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocketReducer.rockets);
+
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, rockets]);
+
+  const ReserveBtn = (id) => {
+    dispatch(rocketsReserve(id));
+  };
+
+  return (
+    <div className="inside">
+      <h1>Rockets</h1>
+
+      {rockets.map(({
+        id, reserved,
+      }) => (
+        <div key={id}>
+          {reserved && <button type="button">Reserved</button>}
+          <button type="button" onClick={() => ReserveBtn(id)}>
+            {reserved && <span>Cancel Reservation</span>}
+            {!reserved && <span> Reserve</span>}
+          </button>
+        </div>
+
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
